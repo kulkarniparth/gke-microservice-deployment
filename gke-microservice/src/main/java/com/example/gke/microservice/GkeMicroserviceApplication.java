86d7c219776629d6ef3afee40c67ad665cae8a78
kulkarniparth <parthkulkarni998@gmail.com>
2023-02-15 18:42:50 +0530

    baseline code

---
+++ gke-microservice/src/main/java/com/example/gke/microservice/GkeMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.gke.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GkeMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GkeMicroserviceApplication.class, args);
+	}
+
+}
---
+++ gke-microservice/src/test/java/com/example/gke/microservice/GkeMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gke.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GkeMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
