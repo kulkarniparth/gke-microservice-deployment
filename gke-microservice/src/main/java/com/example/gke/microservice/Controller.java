d05842ba332c2eb1f8abb39d553208da70363845
kulkarniparth <parthkulkarni998@gmail.com>
2023-02-23 21:08:44 +0530

    bucket controller

--- gke-microservice/src/main/java/com/example/gke/microservice/Controller.java
+++ gke-microservice/src/main/java/com/example/gke/microservice/Controller.java
@@ -6,8 +6,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+
+    @Value("gs://file-upload-bucket-parth-kulkarni-unique-hopefully/hello2.txt")
+    private Resource gcsFile;
+
     @GetMapping("/{name}")
     public String sayHello(@PathVariable String name) {
-        return "Hello "+name+" !!";
+        return "Hello "+name+" !! "+StreamUtils.copyToString(
+                gcsFile.getInputStream(),
+                Charset.defaultCharset()
+        );
     }
+
+    @PostMapping
+    public String writeGcs(@RequestBody String data) throws IOException {
+        try (OutputStream os = ((WritableResource) gcsFile).getOutputStream()) {
+            os.write(data.getBytes());
+        }
+        return "file was updated\n";
+    }
+
 }
